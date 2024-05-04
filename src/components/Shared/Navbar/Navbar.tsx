bcabdf5ae15d8965aa025281c3b9dc08e8659551
raihanwebmaster <raihanemon2015@gmail.com>
2024-05-04 17:41:52 +0200

    login and register page implement

--- src/components/Shared/Navbar/Navbar.tsx
+++ src/components/Shared/Navbar/Navbar.tsx
@@ -1,7 +1,13 @@
+"use client";
 import { Box, Button, Container, Stack, Typography } from "@mui/material"
+import dynamic from "next/dynamic";
 import Link from "next/link"
 
 const Navbar = () => {
+  const AuthButton = dynamic(
+    () => import("@/components/UI/AuthButton/AuthButton"),
+    { ssr: false }
+  );
   return (
     <Container>
       <Stack py={2} direction={"row"} justifyContent={"space-between"} alignItems={"center"}>
@@ -15,7 +21,7 @@ const Navbar = () => {
           <Typography component={Link} href="/daignostics">Diagnostics</Typography>
           <Typography component={Link} href="/ngos">NGOs</Typography>
         </Stack>
-        <Button component={Link} href="/login">Login</Button>
+        <AuthButton/>
       </Stack>
     </Container>
   )
